e1ed0dcee7d6aeaaf619d54af1710f3b072513d4
IndexHTML01 <piyush.singh100805@gmail.com>
2024-05-18 17:05:18 +0530

    Some more necessary files.

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelectorAll("button")
+btn[0].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[1].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[2].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[3].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[4].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[5].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+btn[6].addEventListener("click",function(){
+    window.location.replace("portfolio1.html")
+})
+
